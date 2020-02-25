2c11a44015749c87d8698095c39cea291bcf729a
ruxioana <ruxy24@gmail.com>
2020-02-25 12:52:56 +0200

    hangman

--- javascript/exercises js/hangman/hangman.js
+++ javascript/exercises js/hangman/hangman.js
@@ -6,11 +6,11 @@ var game = {
     start: function() {
         this.word = this.randomWord();
     },
-    randomWord = function() {
+    randomWord: function() {
         let index = Math.floor(Math.random() * this.words.length);
         return this.words[index];
     },
-    tryLetter = function(letter) {
+    tryLetter: function(letter) {
         this.letters.push(letter);
         if(!this.word.split('').includes(letter)) { /* spargem stringul litera cu litera ca sa cautam daca litera apasata este in cuvant */
             this.triesLeft--;
@@ -21,7 +21,7 @@ var game = {
     },
     hasWon: function() {
         return this.word.split('')
-                   .every(letter => this.letters.includes(letter)); /* toate literele din cuvant sunt incluse in letters */
+                        .every(letter => this.letters.includes(letter)); /* toate literele din cuvant sunt incluse in letters */
     }
 }
 
@@ -29,5 +29,39 @@ var game = {
 function renderLetters() { 
     document.getElementById("letters").innerText = game.letters.join(' ');
 }
+
+function renderWord() {
+    let text = game.word.split('')
+                        .map(letter => game.letters.includes(letter) ? letter : '_') /* aici este un if la semnul intrebarii */
+                        .join(' ');
+    document.getElementById("word").innerText = text;
+}
+
+function renderDrawing() {
+    let parts = ['head', 'body', 'left_arm', 'right_arm', 'left_leg', 'right_leg'];
+    let visible = parts.slice(0, 6 - game.triesLeft);
+    let invisible = parts.slice(6 - game.triesLeft);
+    visible.forEach(name => document.getElementById(name).style.visibility = 'visible'); /* aici luam din css si modificam visibility */
+}
+
+function renderView() {
+    renderLetters();
+    renderWord();
+    renderDrawing();
+}
+
+window.addEventListener('keydown', function(event) {
+    if (game.isDead() || game.hasWon()) return;
+    game.tryLetter(event.key);
+    renderView();
+})
+
+game.start();
+renderView();
+
+/* game.word = 'APPLE';     in partea asta testam fiecare bucata in parte sa vedem daca se afiseaza bine
 game.letters = ['A', 'E'];
+game.triesLeft = 4;
 renderLetters();
+renderWord();
+renderDrawing(); */ 
