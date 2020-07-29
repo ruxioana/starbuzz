d38a463dcdfc76cac687264bf327aa977d4472e0
ruxioana <ruxy24@gmail.com>
2020-07-30 01:12:49 +0300

    application start

--- adv_js/exercises/callback/circle.js
+++ adv_js/exercises/callback/circle.js
@@ -1,8 +1,14 @@
 function go() {
-    showCircle(150, 150, 100, div => {
+    /* showCircle(150, 150, 100, div => {
         div.classList.add('message')
         div.append('hello, world!')
-    })
+    }) */
+    showCircleWithPromise(150, 150, 100).then(
+        div => {
+            div.classList.add('message')
+            div.append('hello, world!')
+        }
+    )
 }
 
 function showCircle(x, y, r, callback) {
@@ -22,4 +28,25 @@ function showCircle(x, y, r, callback) {
             callback(div)
         })
     }, 0)
+}
+
+function showCircleWithPromise(x, y, r) {
+    let div = document.createElement('div')
+    div.style.width = 0
+    div.style.height = 0
+    div.style.left = x + 'px'
+    div.style.top = x + 'px'
+    div.className = 'circle'
+    document.body.append(div)
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            div.style.width = r * 2 + 'px'
+            div.style.height = r * 2 + 'px'
+            div.addEventListener('transitionend', function handler() {
+                div.removeEventListener('transitionend', handler)
+                resolve(div)
+            })
+        }, 0)
+    })
 }
